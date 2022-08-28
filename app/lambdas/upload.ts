daf0268fa2ecbe6d1205a46cd87696c1fcc7a8a1
Craig Franklin <craigjfranklin@gmail.com>
2022-08-28 21:01:18 +1000

    Add Chrome AWS Lambda Layer
    This is a bit nicer than installing a full version of headless
    Chrome to AWS Lambda. The package is installed as a devDependency,
    because we will be installing from the layer in production.

--- app/lambdas/upload.ts
+++ app/lambdas/upload.ts
@@ -1,9 +1,13 @@
 import puppeteer from "puppeteer"
+import chromium from "@sparticuz/chrome-aws-lambda"
 
 const UPLOADER_URL = "https://family-album.com/web/uploader"
 
 export const upload = async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    args: chromium.args,
+  })
 
   try {
     const page = await browser.newPage()
