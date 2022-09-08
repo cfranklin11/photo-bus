c1045f9f65951cfdefd57ee9b7cce46ce8ac4f04
Craig Franklin <craigjfranklin@gmail.com>
2022-09-08 22:16:21 +1000

    Fix types in tests

--- tests/lambdas/hello.test.ts
+++ tests/lambdas/hello.test.ts
@@ -1,8 +1,23 @@
 import { hello } from "../../app/lambdas/hello.js"
 
+const context = {
+  callbackWaitsForEmptyEventLoop: true,
+  functionName: "hello",
+  functionVersion: "1",
+  invokedFunctionArn: "arn",
+  memoryLimitInMB: "42",
+  awsRequestId: "id",
+  logGroupName: "group",
+  logStreamName: "stream",
+  getRemainingTimeInMillis: () => 42,
+  done: (error?: Error, result?: any) => {},
+  fail: (error: Error | string) => {},
+  succeed: (messageOrObject: any) => {},
+}
+
 describe("hello", () => {
   it("returns a response", async () => {
-    const response = hello()
+    const response = await hello(null, context, () => {})
     expect(response.statusCode).toEqual(200)
   })
 })
