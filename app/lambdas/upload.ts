fee590c18600e6016b4c7d62e8ab880855e65e40
Craig Franklin <craigjfranklin@gmail.com>
2022-09-09 22:15:14 +1000

    Log into family album

--- app/lambdas/upload.ts
+++ app/lambdas/upload.ts
@@ -5,8 +5,9 @@ import chromium from "@sparticuz/chrome-aws-lambda"
 
 import type { Handler } from "aws-lambda"
 
-const { NODE_ENV } = process.env
+const { NODE_ENV, ACCOUNT_EMAIL, ACCOUNT_PASSWORD } = process.env
 const UPLOADER_URL = "https://family-album.com/web/uploader"
+const REQUIRED_ENV_VARS = [ACCOUNT_EMAIL, ACCOUNT_PASSWORD]
 
 const launchBrowser = async () => {
   if (NODE_ENV === "production")
@@ -22,11 +23,25 @@ const launchBrowser = async () => {
 }
 
 export const upload: Handler = async () => {
+  if (!ACCOUNT_EMAIL || !ACCOUNT_PASSWORD) {
+    const missingEnvVars = REQUIRED_ENV_VARS.filter((envVar) => !envVar)
+
+    throw Error(
+      `Missing values for the following required env vars: ${missingEnvVars.join(
+        ", "
+      )}`
+    )
+  }
+
   const browser = await launchBrowser()
 
   try {
     const page = await browser.newPage()
     await page.goto(UPLOADER_URL)
+    await page.type("input[name=email]", ACCOUNT_EMAIL)
+    await page.type("input[name=password]", ACCOUNT_PASSWORD)
+
+    await page.clickAndWaitForNavigation("button[type=submit]")
     const content = await page.content()
     console.log(content)
   } finally {
